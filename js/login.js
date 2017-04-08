1c90f9d63361427fbde0def241e5dc7c52c00f65
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-04-07 21:25:18 -0300

    Funcao de login e register estao funcionando O usuario ja consegue se registrar pelo email e logar com ele, porem precisamos verificar o segundo password na hora de registrar

--- js/login.js
+++ js/login.js
@@ -12,4 +12,49 @@
 		$(".login-form").css("display", "block");
 	})
 
+
+	$("#login").click(function(){
+
+
+		var emailInput = $("#emailInput").val();
+		var passwordInput = $("#passwordInput").val();
+
+		console.log([emailInput, passwordInput]);
+
+
+		var trySignIn = firebase.auth().signInWithEmailAndPassword(emailInput, passwordInput);
+
+		trySignIn.then(function(auth){
+			alert("Conseugi logar");
+		}, function(error){
+			alert("Não conseugi logar");
+		});
+	})
+
+	$("#register").click(function(){
+
+
+		var emailInput = $("#emailInput").val();
+		var passwordInput = $("#passwordInput").val();
+
+		console.log([emailInput, passwordInput]);
+
+		firebase.auth()
+		.createUserWithEmailAndPassword(emailInput, passwordInput)
+		.then(function(user){
+			// Already registrated, just have to login
+			// loginCtrl.signingUp = false;
+			// loginCtrl.login();
+			alert("registrated");
+		},function(error){
+			if(error.code == 'auth/email-already-in-use')
+				alert("O email escolhido já esta em uso");
+			else
+				alert("Não consegui realizar o cadastro, por favor tente novamente");
+		})
+	})
+
+
+
+
 })(jQuery);
