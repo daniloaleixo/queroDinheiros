7f244e6375f3db7b7f7a1c9cda4649953c5b7aae
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-05-02 23:37:21 -0300

    Adiciona um exemplo de doughtnut chart
    - Atualiza o database exemplo pra ter mais tipos de gastos no resumo do mês

---
+++ js/spendingPieChart.js
@@ -0,0 +1,44 @@
+var randomScalingFactor = function() {
+    return Math.round(Math.random() * 100);
+};
+
+var config = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+            ],
+            backgroundColor: [
+                window.chartColors.red,
+                window.chartColors.orange,
+                window.chartColors.yellow,
+                window.chartColors.green,
+                window.chartColors.blue,
+            ],
+            label: 'Dataset 1'
+        }],
+        labels: [
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue"
+        ]
+    },
+    options: {
+        responsive: true
+    }
+};
+
+window.onload = function() {
+    var ctx = document.getElementById("chart-area").getContext("2d");
+    window.myPie = new Chart(ctx, config);
+};
+
+
+var colorNames = Object.keys(window.chartColors);
