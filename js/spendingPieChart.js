55b1c58cedb59065aa7cb04c807b8157da1cdf1c
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-05-02 23:59:37 -0300

    PEgando o valor corretamente do gasto mensal e mostrando no grafico
    Agora tem um grafico de doughnut para mostrar os gastos do ultimo mes =)

--- js/spendingPieChart.js
+++ js/spendingPieChart.js
@@ -1,24 +1,22 @@
-var randomScalingFactor = function() {
-    return Math.round(Math.random() * 100);
-};
+var data = {};
+
 
 var config = {
     type: 'doughnut',
     data: {
         datasets: [{
             data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
+                0,
+                0,
+                0,
+                0,
+                0,
             ],
             backgroundColor: [
-                window.chartColors.red,
-                window.chartColors.orange,
-                window.chartColors.yellow,
-                window.chartColors.green,
-                window.chartColors.blue,
+                '#11303B',
+                '#266D73',
+                '#45EDDC',
+                '#33C79B',
             ],
             label: 'Dataset 1'
         }],
@@ -36,8 +34,50 @@ var config = {
 };
 
 window.onload = function() {
+    var database = firebase.database();
+    var user = CookieHandler.checkCookie();
+
+    // Date
+    var today = new Date();
+    var dd = today.getDate().toString();
+    var mm = today.getMonth() + 1;
+    if (mm < 10) mm = "0" + mm.toString();
+    var yyyy = today.getFullYear().toString();
+
+
+
     var ctx = document.getElementById("chart-area").getContext("2d");
-    window.myPie = new Chart(ctx, config);
+
+    firebase.database().ref().child(user.uid)
+                            .child(yyyy)
+                            .child(mm)
+                            .child("summary")
+                            .child("spendingPerCategories")
+                            .on('value', function(snapshot) {
+
+        var sortedSpendings = snapshot.val().sort(function(a, b){
+            return b.amount - a.amount;
+        })
+
+        // console.log(sortedSpendings);
+
+        var spendingsName = [];
+        var spendingsAmount = [];
+
+        sortedSpendings.forEach(function(spending){
+            spendingsName.push(spending.name);
+            spendingsAmount.push(spending.amount);
+        })
+
+        // Put in the config for the chart
+        config["data"]["labels"] = spendingsName;
+        config["data"]["datasets"][0]["data"] = spendingsAmount;
+
+        window.myPie = new Chart(ctx, config);
+
+        
+    })
+
 };
 
 
