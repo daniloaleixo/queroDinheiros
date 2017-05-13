f174a9b5fcc62893586de9d18f46e3a8b2969504
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-05-13 10:48:40 -0300

    Consegui pegar os gastos de todos os meses
    Preciso estruturar os gastos e limpar os dados

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -38,19 +38,41 @@ for i in range(1, len(mesesLinha)- 1):
 arrayGastos = [[]] * len(meses)
 
 
-gastos = linhas[2].find_all('td')
+todosGastosHtml = [[]] * 27
+for i in range(2, 25):
+	todosGastosHtml[i - 2] = linhas[i].find_all('td')
+
+
 indiceMes = 0
-for i in range(1, len(gastos) - 1, 3):
-	desc = gastos[i].string
-	amount = gastos[i + 1].string
-	tag = gastos[i + 2].string
-	if desc != None and amount != None and tag != None and indiceMes < len(meses):
+for j in range(0, len(meses)):
+	
+	gastoMes = []
+	primeira_linha_tem_o_debito = 1
+
+	for i in range(0, 27):
 		novoGasto = {}
-		novoGasto['description'] = desc
-		novoGasto['amount'] = amount
-		novoGasto['tag'] = tag
-		arrayGastos[indiceMes].append(novoGasto)
-		indiceMes = indiceMes + 1
+		if len(todosGastosHtml[i]) > 0:
+			desc = todosGastosHtml[i][(j * 3) + primeira_linha_tem_o_debito].string
+			amount = todosGastosHtml[i][(j * 3) + 1 + primeira_linha_tem_o_debito].string
+			tag = todosGastosHtml[i][(j * 3) + 2 + primeira_linha_tem_o_debito].string
+			if desc != None and amount != None and tag != None:
+				novoGasto['description'] = desc
+				novoGasto['amount'] = amount
+				novoGasto['tag'] = tag
+				gastoMes.append(novoGasto)
+
+		if i > 0: primeira_linha_tem_o_debito = 0
+
+	arrayGastos[indiceMes] = gastoMes
+	indiceMes = indiceMes + 1
+
+
+# print 'rsultado final'
+# print gastoMes
+print meses[0], arrayGastos[0]
+print meses[len(meses)- 3], arrayGastos[len(meses) - 3]
+print meses[len(meses)- 1], arrayGastos[len(meses) - 1]
+
 
 
 
