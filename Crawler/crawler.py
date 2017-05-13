93a4d5274817918a820dac15f139ef4184b91d88
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-05-13 00:23:02 -0300

    Tentando fazer o parser dos meus gastos

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,28 +1,63 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import os
-from HTMLParser import HTMLParser
+from bs4 import BeautifulSoup
+import codecs
 
-
-f = open("Gastos.html")
+f = codecs.open('Gastos.html', 'r', encoding='utf-8')
 content = f.read()
 f.close()
 
+saida = codecs.open('gastos.json', 'w', encoding='utf-8')
+
 # print content
 
+# parse HTML
+soup = BeautifulSoup(content, 'html.parser')
+# print(soup.prettify())
+
+tbody = soup.table.tbody
+
+linhas = tbody.find_all('tr')
+
+# Linha com os meses
+mesesLinha = linhas[0].find_all('td')
 
 
-# create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print "Encountered a start tag:", tag
+meses = []
 
-    def handle_endtag(self, tag):
-        print "Encountered an end tag :", tag
+# MESES
+# exclude first line
+for i in range(1, len(mesesLinha)- 1):
+	mes = mesesLinha[i].string
+	if mes != None:
+		meses.append(mesesLinha[i].string)
 
-    def handle_data(self, data):
-        print "Encountered some data  :", data
 
-# instantiate the parser and fed it some HTML
-parser = MyHTMLParser()
-parser.feed(content)
+arrayGastos = [[]] * len(meses)
+
+
+gastos = linhas[2].find_all('td')
+indiceMes = 0
+for i in range(1, len(gastos) - 1, 3):
+	desc = gastos[i].string
+	amount = gastos[i + 1].string
+	tag = gastos[i + 2].string
+	if desc != None and amount != None and tag != None and indiceMes < len(meses):
+		novoGasto = {}
+		novoGasto['description'] = desc
+		novoGasto['amount'] = amount
+		novoGasto['tag'] = tag
+		arrayGastos[indiceMes].append(novoGasto)
+		indiceMes = indiceMes + 1
+
+
+
+
+
+
+
+
+
+
